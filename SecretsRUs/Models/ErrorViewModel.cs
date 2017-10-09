db35103be430dbda18c27cd1f54692efad59bc14
Mark Taylor <mark.taylor@Betfred.com>
2017-10-09 21:26:44 +0100

    Initial build from new project

---
+++ SecretsRUs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecretsRUs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
