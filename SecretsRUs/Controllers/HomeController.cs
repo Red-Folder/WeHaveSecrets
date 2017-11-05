f8fa8705589dce51c6d5a9e4083643d153a1c6cf
Mark Taylor <mark.taylor@Betfred.com>
2017-11-05 21:38:16 +0000

    Clean up and removal of auto created functionality

--- SecretsRUs/Controllers/HomeController.cs
+++ SecretsRUs/Controllers/HomeController.cs
@@ -16,21 +16,6 @@ namespace SecretsRUs.Controllers
             return View();
         }
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
