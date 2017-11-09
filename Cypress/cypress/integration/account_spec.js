7a1ae2c6cb7eb8a84e8a71594f9b626a44183062
Mark Taylor <mark.taylor@Betfred.com>
2017-11-09 21:35:41 +0000

    Added a BDD test for public share

--- Cypress/cypress/integration/account_spec.js
+++ Cypress/cypress/integration/account_spec.js
@@ -17,7 +17,7 @@ describe('As a SecretsRUs customer', function(){
         cy.logout()
     })
 
-    it('I should be able to register, and add a secret', function(){
+    it('I should be able to register, and add a secret, collect the link and then access anonymously', function(){
         cy.visit('http://localhost:44353/')
 
         let username = 'user' + (new Date()).toISOString().replace(/[^0-9]/g, "")
@@ -39,8 +39,28 @@ describe('As a SecretsRUs customer', function(){
         // Validate that the secret is there
         cy.get('.secrets-link')
             .click()
-        cy.get('.secrets-list')
-            .contains(secretValue)
+        let secretList = cy.get('.secrets-list')
+        secretList.contains(secretValue)
+
+        // Navigate to the Public Share
+        let shareLink = "";
+        cy.get('.secrets-list').within(function() {
+            cy.get(".panel:contains('" + secretKey + "')").within(function() {
+                cy.get("a").then(($link) => {
+                    cy.visit($link.prop('href'))
+                        //.then(() => shareLink = cy.url())
+                })
+            })
+        })
+
+        // Store the current public share
+        cy.url().then((link) => shareLink = link)
+
+        // Logout & open the share as a public user
+        cy.logout()
+            .then(() => {
+                cy.visit(shareLink)
+            })
     })
 
 })
